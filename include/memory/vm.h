464eac3116522bd7a6e21a23f4a5015f93718dd8
XinyiChen <cxinyic@sjtu.edu.cn>
2020-05-12 21:25:10 -0400

    u

--- include/memory/vm.h
+++ include/memory/vm.h
@@ -170,6 +170,8 @@ int do_brk(struct lego_task_struct *p, unsigned long addr,
 
 void unmap_vmas(struct vm_area_struct *vma, unsigned long start_addr,
 		unsigned long end_addr);
+unsigned long do_sp_alloc(struct lego_task_struct *p, unsigned long len);
+int do_sp_free(struct lego_mm_struct *mm, unsigned long start, size_t len);
 
 /*
  * Look up the first VMA which intersects the interval start_addr..end_addr-1,
--- managers/memory/handle_sp.c
+++ managers/memory/handle_sp.c
@@ -19,53 +19,7 @@
 
 
 
-unsigned long do_sp_alloc(struct lego_task_struct *p, unsigned long len){
-    if (down_write_killable(&p->mm->mmap_sem))
-		return -EINTR;
-    struct lego_mm_struct *mm = p->mm;
-    struct vm_area_struct *vma, *prev;
-    struct rb_node **rb_link, *rb_parent;
-    unsigned long flags;
-    unsigned long newaddr;
-    pgoff_t pgoff;
-    len = PAGE_ALIGN(len);
-    if (!len){
-        return -1;
-    }
-    flags = VM_READ | VM_WRITE | mm->def_flags;
 
-    /* sp alloc */
-    newaddr = get_unmapped_area(p,NULL,NULL,len,0,MAP_ANONYMOUS,1);
-    if (offset_in_page(newaddr))
-		return newaddr;
-    while (find_vma_links(mm, newaddr, newaddr + len, &prev, &rb_link,
-			      &rb_parent)) {
-		if (do_munmap(mm, newaddr, len))
-			return -ENOMEM;
-	}
-    vma = vma_merge(mm, prev, addr, addr + len, flags,
-			NULL, pgoff);
-	if (vma)
-		goto out;
-    vma = kzalloc(sizeof(*vma), GFP_KERNEL);
-	if (!vma)
-		return -ENOMEM;
-    pgoff = newaddr >> PAGE_SHIFT;
-
-	vma->vm_mm = mm;
-	vma->vm_start = newaddr;
-	vma->vm_end = newaddr + len;
-	vma->vm_pgoff = pgoff;
-	vma->vm_flags = flags;
-	vma->vm_page_prot = vm_get_page_prot(flags);
-	vma_link(mm, vma, prev, rb_link, rb_parent);
-
-out:
-    mm->total_vm += len >> PAGE_SHIFT;
-	mm->data_vm += len >> PAGE_SHIFT;
-    up_write(&p->mm->mmap_sem);
-	return newaddr;
-}
 void handle_p2m_sp_alloc(struct p2m_sp_alloc_struct *payload,
 		       struct common_header *hdr, struct thpool_buffer *tb){
     
@@ -102,59 +56,7 @@ void handle_p2m_sp_alloc(struct p2m_sp_alloc_struct *payload,
     replicate_vma(tsk,REPLICATE_SP,reply->ret_sp,len,0,0);
 
 }
-/* very similar to do_munmap() function; but we don't need to remove page table*/
-int do_sp_free(struct lego_mm_struct *mm, unsigned long start, size_t len)
-{
 
-    unsigned long end;
-	struct vm_area_struct *vma, *prev, *last;
-
-    if ((offset_in_page(start)) || start > TASK_SIZE || len > TASK_SIZE-start)
-		return -EINVAL;
-
-	len = PAGE_ALIGN(len);
-	if (len == 0)
-		return -EINVAL;
-    vma = find_vma(mm, start);
-    if(!vma){
-        return 0;
-    }
-    prev = vma->vm_prev;
-    end = start + len;
-    if (vma->vm_start >=end){
-        return 0;
-    }
-    if (start > vma->vm_start) {
-		int error;
-
-		if (end < vma->vm_end && mm->map_count >= sysctl_max_map_count)
-			return -ENOMEM;
-
-		error = __split_vma(mm, vma, start, 0);
-		if (error)
-			return error;
-		prev = vma;
-
-	}
-    last = find_vma(mm, end);
-	if (last && end > last->vm_start) {
-		int error = __split_vma(mm, last, end, 1);
-		if (error)
-			return error;
-	}
-	vma = prev ? prev->vm_next : mm->mmap;
-    detach_vmas_to_be_unmapped(mm, vma, prev, end);
-    remove_vma_list(mm, vma);
-
-    return 0;
-
-
-
-
-
-
-
-}
 
 void handle_p2m_sp_free(struct p2m_sp_free_struct *payload,
 		       struct common_header *hdr, struct thpool_buffer *tb)

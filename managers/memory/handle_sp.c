6520ff5f97576f4493399d5227c080292b4dc9b8
XinyiChen <cxinyic@sjtu.edu.cn>
2020-05-12 21:17:11 -0400

    u

--- managers/memory/handle_sp.c
+++ managers/memory/handle_sp.c
@@ -17,42 +17,7 @@
 #include <memory/replica.h>
 #include <memory/thread_pool.h>
 
-void handle_p2m_sp_alloc(struct p2m_sp_alloc_struct *payload,
-		       struct common_header *hdr, struct thpool_buffer *tb){
-    
-    u32 nid = hdr->src_nid;
-    u32 pid = payload->pid;
-    unsigned long len = payload->len;
-    struct lego_task_struct *tsk;
-    struct lego_mm_struct *mm;
-    struct p2m_sp_alloc_reply_struct *reply;
 
-    s64 newaddr;
-
-    reply = thpool_buffer_tx(tb);
-    tb_set_tx_size(tb, sizeof(*reply));
-
-    tsk = find_lego_task_by_pid(nid, pid);
-	if (unlikely(!tsk)) {
-		reply->ret_sp = RET_ESRCH;
-		return;
-	}
-    debug_dump_vm_all(tsk->mm, 1);
-
-    mm = tsk->mm;
-    
-    
-    newaddr = do_sp_alloc(tsk,len);
-    tb_set_tx_size(tb, sizeof(long));
-    if (unlikely(newaddr < 0)) {
-		reply->ret = newaddr;
-		return;
-	}
-    reply->ret = 0;
-    reply->ret_sp = newaddr;
-    replicate_vma(tsk,REPLICATE_SP,reply->ret_sp,len,0,0);
-    debug_dump_vm_all(mm, 0);
-}
 
 unsigned long do_sp_alloc(struct lego_task_struct *p, unsigned long len){
     if (down_write_killable(&p->mm->mmap_sem))
@@ -102,6 +67,42 @@ out:
     up_write(&p->mm->mmap_sem);
 	return newaddr;
 }
+void handle_p2m_sp_alloc(struct p2m_sp_alloc_struct *payload,
+		       struct common_header *hdr, struct thpool_buffer *tb){
+    
+    u32 nid = hdr->src_nid;
+    u32 pid = payload->pid;
+    unsigned long len = payload->len;
+    struct lego_task_struct *tsk;
+    struct lego_mm_struct *mm;
+    struct p2m_sp_alloc_reply_struct *reply;
+
+    s64 newaddr;
+
+    reply = thpool_buffer_tx(tb);
+    tb_set_tx_size(tb, sizeof(*reply));
+
+    tsk = find_lego_task_by_pid(nid, pid);
+	if (unlikely(!tsk)) {
+		reply->ret_sp = RET_ESRCH;
+		return;
+	}
+
+
+    mm = tsk->mm;
+    
+    
+    newaddr = do_sp_alloc(tsk,len);
+    tb_set_tx_size(tb, sizeof(long));
+    if (unlikely(newaddr < 0)) {
+		reply->ret = newaddr;
+		return;
+	}
+    reply->ret = 0;
+    reply->ret_sp = newaddr;
+    replicate_vma(tsk,REPLICATE_SP,reply->ret_sp,len,0,0);
+
+}
 
 void handle_p2m_sp_free(struct p2m_sp_free_struct *payload,
 		       struct common_header *hdr, struct thpool_buffer *tb)
@@ -109,7 +110,7 @@ void handle_p2m_sp_free(struct p2m_sp_free_struct *payload,
     u32 nid = hdr->src_nid;
     u32 pid = payload->pid;
     unsigned long len = payload->len;
-    unsigned long len = payload->addr;
+    unsigned long addr = payload->addr;
     struct lego_task_struct *tsk;
     struct lego_mm_struct *mm;
     struct p2m_sp_free_reply_struct *reply;
@@ -122,10 +123,10 @@ void handle_p2m_sp_free(struct p2m_sp_free_struct *payload,
 		reply->ret = RET_ESRCH;
 		return;
 	}
-	debug_dump_vm_all(tsk->mm, 1);
+
 
 	mm = tsk->mm;
-    if (down_write_killable(&p->mm->mmap_sem)){
+    if (down_write_killable(&tsk->mm->mmap_sem)){
         reply->ret = RET_EINTR;
         return;
 
@@ -134,7 +135,7 @@ void handle_p2m_sp_free(struct p2m_sp_free_struct *payload,
 
     up_write(&mm->mmap_sem);
     replicate_vma(tsk,REPLICATE_SP,addr,len,0,0);
-    debug_dump_vm_all(tsk->mm, 0);
+
                
 }
 /* very similar to do_munmap() function; but we don't need to remove page table*/
